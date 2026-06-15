d34c8ac732f58808ff0b3d74911479aadde1ab0e
JAGDISH <js8724285@gmail.com>
2026-06-15 12:27:16 +0530

    [Jagdish] Add Kingdom strory question

---
+++ core-java-practice/Story_Questions/KingdomTax.java
@@ -0,0 +1,36 @@
+import java.util.*;
+
+public class KingdomTax {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        double[] income = new double[10];
+        double totalTax = 0;
+
+        for (int i = 0; i < 10; i++) {
+            System.out.print("Enter income of citizen " + (i + 1) + ": ");
+            income[i] = sc.nextDouble();
+
+            double tax;
+            String bracket;
+
+            if (income[i] < 10000) {
+                tax = income[i] * 0.05;
+                bracket = "5% Tax Bracket";
+            } else if (income[i] <= 50000) {
+                tax = income[i] * 0.15;
+                bracket = "15% Tax Bracket";
+            } else {
+                tax = income[i] * 0.30;
+                bracket = "30% Tax Bracket";
+            }
+
+            System.out.println("Bracket: " + bracket);
+            System.out.println("Tax Amount: " + tax);
+
+            totalTax += tax;
+        }
+
+        System.out.println("\nTotal Tax Collected = " + totalTax);
+    }
+}
